c5fbeff5dcf6c58d586f517ea9980ee4ae3bc3b8
yedukondalu77 <yedukondalunadakuditi21@gmail.com>
2024-12-19 10:51:02 +0530

    7hills latest build

--- src/Components/Api.js
+++ src/Components/Api.js
@@ -6,8 +6,11 @@ axios.defaults.xsrfHeaderName='x-csrftoken'
 
 
 const client = axios.create(
+    // {
+    //     baseURL: "http://192.168.29.251:8000"
+    // }
     {
-        baseURL: "http://192.168.29.251:8000"
+        baseURL: "http://localhost:8000"
     }
 )
 
